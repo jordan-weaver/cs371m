8a2be8324161f377af584083344adb1ee4b4d222
jordan-weaver <Jordan.Weaver@utexas.edu>
2016-11-04 20:59:29 -0500

    added xml back

--- PaperPlanes/app/src/main/java/cs371m/paperplanes/JoinGameActivity.java
+++ PaperPlanes/app/src/main/java/cs371m/paperplanes/JoinGameActivity.java
@@ -40,7 +40,6 @@ public class JoinGameActivity extends AppCompatActivity {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 deviceList.add(device);
                 mArrayAdapter.notifyDataSetChanged();
-                Toast.makeText(context, "Added " + device.getName() + " to deviceList", Toast.LENGTH_SHORT).show();
                 // Add the name and address to an array adapter to show in a ListView
             }
         }
